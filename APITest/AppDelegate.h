d11493568ea9fa5b689db4551b3502bb14398d4e
keepfocused <admin@mac-admin.local>
2016-09-15 11:23:27 +0300

    Initial Commit

---
+++ APITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APITest
+//
+//  Created by Admin on 15.09.16.
+//  Copyright © 2016 Galiev Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APITest
+//
+//  Created by Admin on 15.09.16.
+//  Copyright © 2016 Galiev Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
